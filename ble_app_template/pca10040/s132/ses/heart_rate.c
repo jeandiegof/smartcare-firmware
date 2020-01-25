d7f7c1e960d853519f8a1430e600b45d94b48eaa
Leoni Mota Loris <leoniloris1@hotmail.com>
2020-01-25 19:23:33 -0300

    Filters.

--- ble_app_template/pca10040/s132/ses/heart_rate.c
+++ ble_app_template/pca10040/s132/ses/heart_rate.c
@@ -6,6 +6,8 @@
 #include "max30101.h"
 #endif
 
+#define POLE 0.05
+
 void heart_rate_start(void) {
 #ifdef MAX30100
     max30100_init();
@@ -16,13 +18,30 @@ void heart_rate_start(void) {
 #endif
 }
 
-static void process_new_sample(uint32_t sample) {
-    static uint32_t _previous_sample = 0;
-    dcFilter_t filtered;
-    filtered.w = x + alpha * prev_w;
-    filtered.result = filtered.w - prev_w;
+static float high_pass_filter(float sample) {
+    static float _previous_sample = 0.0;
+    static float _previous_retult = 0.0;
+    _previous_retult = sample - _previous_sample * POLE;
+    _previous_sample = sample;
+    return _previous_retult;
+}
 
-    return filtered;
+// http://www.schwietering.com/jayduino/filtuino/
+// order=1 alpha1=0.12
+// Sampling rate 50 hz
+static float butterworth_filter(float sample) {
+    static float v0 = 0.0;
+    static float v1 = 0.0;
+    v0 = v1;
+    v1 = (2.836306788762870124e-1 * sample) + (0.43273864224742591977 * v0);
+    return (v0 + v1);
+}
+
+static void process_new_sample(uint32_t sample) {
+    float filtered_sample = high_pass_filter((float)sample);
+    filtered_sample = butterworth_filter(filtered_sample);
+
+
 }
 
 void handle_heart_rate_interruption(void) {
